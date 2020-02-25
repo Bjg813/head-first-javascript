d9278a2c4084c7707d0e18d3ac6d11a3532dcaa1
Brennan Grout <brennan@Brennans-iMac.local>
2020-02-24 20:16:01 -0800

    added a custom bark method for spot

--- asynchronise-coding/dog-prototype/scripts/dogPrototype.js
+++ asynchronise-coding/dog-prototype/scripts/dogPrototype.js
@@ -25,6 +25,11 @@ Dog.prototype.wag = function() {
 let fido = new Dog('Fido', 'Mixed', 38);
 let fluffy = new Dog('Fluffy', 'Poodle', 30);
 let spot = new Dog('Spot', 'Chihuahua', 10);
+// Spot wants a bigger bark so we give spot his own custom method instead of inheriting the bark prototype from the Dog constructor
+// The javascript engine will always look in the object instance for a property before it looks into the prototype, so if you want a custom property all you have to do is place it in the object and it will run that property before and it wont look in the prototype for it
+spot.bark = function() {
+    console.log(this.name + ' says WOOF!');
+}
 
 fido.bark();
 fido.run();
