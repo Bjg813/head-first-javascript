e4daee4bf8f95f372d8b14cfa3dee651fed4e9cb
Brennan Grout <brennan@Brennans-iMac.local>
2020-03-20 16:58:14 -0700

    added dog constructor

--- asynchronise-coding/dog-prototype/scripts/dogPrototype.js
+++ asynchronise-coding/dog-prototype/scripts/dogPrototype.js
@@ -22,9 +22,20 @@ Dog.prototype.wag = function() {
     console.log('Wag!');
 };
 
-let fido = new Dog('Fido', 'Mixed', 38);
-let fluffy = new Dog('Fluffy', 'Poodle', 30);
-let spot = new Dog('Spot', 'Chihuahua', 10);
+Dog.prototype.sitting = false;
+
+Dog.prototype.sit = function() {
+    if (this.sitting) {
+        console.log(this.name + ' is already sitting');
+    } else {
+        this.sitting = true;
+        console.log(this.name + ' is now sitting');
+    }
+};
+
+const fido = new Dog('Fido', 'Mixed', 38);
+const fluffy = new Dog('Fluffy', 'Poodle', 30);
+const spot = new Dog('Spot', 'Chihuahua', 10);
 // Spot wants a bigger bark so we give spot his own custom method instead of inheriting the bark prototype from the Dog constructor
 // The javascript engine will always look in the object instance for a property before it looks into the prototype, so if you want a custom property all you have to do is place it in the object and it will run that property before and it wont look in the prototype for it
 spot.bark = function() {
@@ -34,11 +45,16 @@ spot.bark = function() {
 fido.bark();
 fido.run();
 fido.wag();
+fido.sit();
+fido.sit();
+fido.hasOwnProperty('species');
 
 fluffy.bark();
 fluffy.run();
 fluffy.wag();
+fluffy.sit();
 
 spot.bark();
 spot.run();
 spot.wag();
+spot.sit();
